314a71e9cdbc6b5984b414f197d4c540f3018335
eshuaiii <eric.shuai88@gmail.com>
2024-08-08 16:00:54 -0700

    Initial Branch

--- src/workday.ts
+++ src/workday.ts
@@ -1,45 +1,58 @@
-let config;
-let replaceFunctions: VoidFunction[] = [];
-
-let isBlastin = false;
-
-async function importFiles() {
-    const configParser = await import(chrome.runtime.getURL("configParser.js"));
-    config = configParser.config;
-
-    if (config.enableListReplacement) {
-        const listNamesUrl = chrome.runtime.getURL("replace/listNames.js");
-        const replaceListNames = (await import(listNamesUrl)).default;
-        if (replaceListNames) replaceFunctions.push(replaceListNames);
+class Workday {
+    private config: any;
+    private replaceFunctions: VoidFunction[] = [];
+    
+    private isBlastin = false;
+    
+    constructor() {
+        this.addListener()
+        this.setReplacementLoop()
     }
-    if (config.enableTableReplacement) {
-        const tableNamesUrl = chrome.runtime.getURL("replace/tableNames.js");
-        const replaceTableNames = (await import(tableNamesUrl)).default;
-        if (replaceTableNames) replaceFunctions.push(replaceTableNames);
-    }
-    if (config.enableNSIDReplacement) {
-        const nsidNamesUrl = chrome.runtime.getURL("replace/nsidNames.js");
-        const replaceNSIDNames = (await import(nsidNamesUrl)).default;
-        if (replaceNSIDNames) replaceFunctions.push(replaceNSIDNames);
-    }
-}
 
-async function replaceNames() {
-    if (replaceFunctions.length === 0) await importFiles();
-    replaceFunctions.forEach(f => f());
-}
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'startBlastin') {
-        replaceNames()
-        isBlastin = true
-        sendResponse();
-    } else if (message.action === 'stopBlastin') {
-        isBlastin = false
-        sendResponse();
-    } else if (message.action === 'isBlastin?') {
-        sendResponse(isBlastin);
+    private async importFiles() {
+        const configParser = await import(chrome.runtime.getURL("configParser.js"));
+        this.config = configParser.config;
+    
+        if (this.config.enableListReplacement) {
+            const listNamesUrl = chrome.runtime.getURL("replace/listNames.js");
+            const replaceListNames = (await import(listNamesUrl)).default;
+            if (replaceListNames) this.replaceFunctions.push(replaceListNames);
+        }
+        if (this.config.enableTableReplacement) {
+            const tableNamesUrl = chrome.runtime.getURL("replace/tableNames.js");
+            const replaceTableNames = (await import(tableNamesUrl)).default;
+            if (replaceTableNames) this.replaceFunctions.push(replaceTableNames);
+        }
+        if (this.config.enableNSIDReplacement) {
+            const nsidNamesUrl = chrome.runtime.getURL("replace/nsidNames.js");
+            const replaceNSIDNames = (await import(nsidNamesUrl)).default;
+            if (replaceNSIDNames) this.replaceFunctions.push(replaceNSIDNames);
+        }
     }
-});
+    
+    private async replaceNames() {
+        if (this.replaceFunctions.length === 0) await this.importFiles();
+        this.replaceFunctions.forEach(f => f());
+    }
+    
+    private addListener() {
+        chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+            if (message.action === 'startBlastin') {
+                this.replaceNames()
+                this.isBlastin = true
+                sendResponse();
+            } else if (message.action === 'stopBlastin') {
+                this.isBlastin = false
+                sendResponse();
+            } else if (message.action === 'isBlastin?') {
+                sendResponse(this.isBlastin);
+            }
+        });
+    }
 
-setInterval(() => {if (isBlastin) replaceNames();}, 2000);
+    private setReplacementLoop() {
+        setInterval(() => {if (this.isBlastin) this.replaceNames();}, 2000);
+    }
+};
+
+const workday = new Workday();
