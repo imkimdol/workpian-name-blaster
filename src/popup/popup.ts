a264ba88c3041ac56e66c0c151e725e2361de4ab
imkimdol <kimdol@proton.me>
2024-08-14 18:42:19 -0700

    Add docs for popup script

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,4 +1,7 @@
-type ElementContainer = {
+/**
+ * Houses all the elements that will be changed with JavaScript
+ */
+type DynamicElementsContainer = {
     border: HTMLButtonElement;
     blastButton: HTMLButtonElement
 };
@@ -6,7 +9,10 @@ type ElementContainer = {
 const blastText = "<p>Blast!</p>"
 const unblastText = "<p>Unblast</p>"
 
-function getElements(): ElementContainer {
+/**
+ * Creates a `DynamicElementsContainer` with elements retrieved from popup.
+ */
+function getElements(): DynamicElementsContainer {
     const border = document.getElementById('border') as HTMLButtonElement | null;
     const blastButton = document.getElementById('blastButton') as HTMLButtonElement | null;
     if (!border) throw new Error("Border not found!");
@@ -15,6 +21,9 @@ function getElements(): ElementContainer {
     return {border, blastButton};
 }
 
+/**
+ * Fetches the tab ID of the active tab.
+ */
 async function getActiveTabId(): Promise<number> {
     const tabs = await chrome.tabs.query({active: true, currentWindow: true});
     const activeTab = tabs[0];
@@ -24,7 +33,12 @@ async function getActiveTabId(): Promise<number> {
     return id;
 }
 
-function setHTML(elements: ElementContainer, isBlastin: boolean) {
+/**
+ * Changes elements according to blastin status.
+ * @param elements Container with elements to change
+ * @param isBlastin Current blastin status
+ */
+function changeHTMLElements(elements: DynamicElementsContainer, isBlastin: boolean) {
     if (isBlastin) {
         elements.blastButton.innerHTML = unblastText;
         document.body.classList.add("blastin");
@@ -34,7 +48,13 @@ function setHTML(elements: ElementContainer, isBlastin: boolean) {
     }
 }
 
-function addEventListeners(elements: ElementContainer, isBlastin: boolean, activeTabId: number) {
+/**
+ * Adds event listener that triggers state changes on button clicks.
+ * @param elements Container with button to add the listener to
+ * @param isBlastin Current blastin status
+ * @param activeTabId ID of the current tab
+ */
+function addButtonEventListener(elements: DynamicElementsContainer, isBlastin: boolean, activeTabId: number) {
     elements.blastButton.addEventListener('click', () => {
         if (!isBlastin) {
             chrome.tabs.sendMessage(activeTabId, {action: 'startBlastin'}, () => {});
@@ -50,19 +70,22 @@ function addEventListeners(elements: ElementContainer, isBlastin: boolean, activ
     });
 };
 
-async function run() {
+/**
+ * Initializes the popup.
+ */
+async function setup() {
     const elements = getElements();
 
     try {
         const activeTabId = await getActiveTabId();
         const isBlastin = await chrome.tabs.sendMessage(activeTabId, {action: 'isBlastin?'});
 
-        setHTML(elements, isBlastin);
-        addEventListeners(elements, isBlastin, activeTabId);
+        changeHTMLElements(elements, isBlastin);
+        addButtonEventListener(elements, isBlastin, activeTabId);
 
         elements.border.classList.remove("disabled");
         elements.blastButton.disabled = false;
     } catch {} // Do nothing
 };
 
-run();
+setup();
